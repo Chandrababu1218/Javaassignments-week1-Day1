b401fefc0b300db3260431720ca89ee5a3d0c723
Chandrababu1218 <107299643+Chandrababu1218@users.noreply.github.com>
2022-06-11 15:05:40 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,26 @@
+package week1.Day1.classroom;
+
+import java.util.Iterator;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int fact = 1;
+	
+	for (int i=5; i>=1; i--) {
+
+		fact = fact*i;
+		
+		
+	}
+	System.out.println("Factorial value of 5 is :" +fact);
+		
+	
+
+		}
+		}
+	
+
+
