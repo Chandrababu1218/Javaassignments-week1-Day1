2d5737647e2e4a2955967dde17555d2f6a564151
Chandrababu1218 <107299643+Chandrababu1218@users.noreply.github.com>
2022-06-11 17:40:40 +0530

    Newfile

---
+++ Factorial.java
@@ -0,0 +1,39 @@
+package week1.Day1.classroom;
+
+import java.util.Iterator;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// with while statement
+		 
+		/*/ * int num = 5; int i=1; int fact = 1;
+		 * 
+		 * while(i<=num) {
+		 * 
+		 * fact = fact*i; i++;
+		 * 
+		 * }*/
+		
+		// with for loop statement
+		
+		int range = 5;
+		
+		int fact = 1;
+		
+		for(int i=1; i<=range; i++)
+		{
+			fact=fact*i;
+		}
+		
+	System.out.println("Factorial value of 5 is :" +fact);
+		
+	
+
+		}
+		}
+	
+
+
